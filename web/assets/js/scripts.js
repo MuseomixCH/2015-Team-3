4abc8a6249befa194b04822071cdcf188c12e406
Daniel Huf <daniel.huf@swisscom.com>
2015-11-07 21:38:46 +0100

    [TASK] Add remaing chars based on the textarea maxlength

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -6,4 +6,18 @@
 		console.log(mapSettings);
 	});
 
+	var textAreaId = '#tweet-body';
+	var textCountId = '#tweet-count';
+	if($(textAreaId)) {
+		var text_max = $(textAreaId).attr('maxlength');
+
+		$(textCountId).html(text_max);
+
+		$(textAreaId).keyup(function () {
+			var text_length = $(this).val().length;
+			var text_remaining = text_max - text_length;
+			$(textCountId).html(text_remaining);
+		});
+	}
+
 })(jQuery, google, GRES);
